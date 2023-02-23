931708bf8c2d27a227432058a65a1c3b6c2e27c3
Nguyễn Võ Hoàng Phi <phinvh@vuthao.local>
2023-02-23 16:35:42 +0700

    fix

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 import popupModule from "./modules/popupModule.js";
 import burgerMenuModule from "./modules/burgerMenuModule.js";
-import navigationModule from "./modules/navigationModule.js";
 import aosModule from "./modules/aosModule.js";
 import searchModule from "./modules/searchModule.js";
 import swiperModule from "./modules/swiperModule.js";
@@ -10,7 +9,6 @@ import videoModule from "./modules/videoModule.js";
 window.addEventListener("DOMContentLoaded", () => {
   popupModule();
   burgerMenuModule();
-  navigationModule();
   aosModule();
   searchModule();
   swiperModule();
--- js/modules/videoModule.js
+++ js/modules/videoModule.js
@@ -12,18 +12,11 @@ export default function videoModules() {
       var srchref = "https://www.youtube.com/embed/";
 
     if ($(this).data("autoplay") == true) autoplay = "?autoplay=1";
-
-    $("#video-popup-iframe").attr("src", srchref + id + autoplay);
-
-    $("#video-popup-iframe").on("load", function () {
-      $("#video-popup-container").show();
-    });
   });
 
   $("#video-popup-close, #video-popup-overlay").on("click", function (e) {
     $(
       "#video-popup-iframe-container,#video-popup-container,#video-popup-close,#video-popup-overlay"
     ).hide();
-    $("#video-popup-iframe").attr("src", "");
   });
 }
