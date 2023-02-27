29caceedfa7b40eacbc2d370c6325f820af12fbe
Nguyễn Võ Hoàng Phi <phinvh@vuthao.local>
2023-02-27 16:58:47 +0700

    fix

--- js/modules/scrollModules.js
+++ js/modules/scrollModules.js
@@ -2,7 +2,7 @@ export default function scrollModule() {
   var btn = $("#button");
 
   $(window).scroll(function () {
-    if ($(window).scrollTop() > 300) {
+    if ($(window).scrollTop() > 0) {
       btn.addClass("show");
     } else {
       btn.removeClass("show");
