518d68c383d4024953496217227d7390632814a7
Nguyễn Võ Hoàng Phi <phinvh@vuthao.local>
2023-03-06 16:19:01 +0700

    fix\

--- js/modules/showhideModule.js
+++ js/modules/showhideModule.js
@@ -2,7 +2,7 @@ export default function showhideModule() {
   $(document).ready(function () {
     $(".button-change").click(function () {
       $(this).text(function (i, v) {
-        return v === "Xem thêm" ? "Rút gọn" : "Xem thêm";
+        return v === "Xem thêm" ? "" : "";
       });
     });
 
